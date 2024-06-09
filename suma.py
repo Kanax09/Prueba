7c37f3b0b43c94b207c91b5b7a80e939225db1ca
Kanade <celestialwarrior90000@gmail.com>
2024-06-09 14:40:10 -0400

    Se agregaron validaciones

--- suma.py
+++ suma.py
@@ -1,7 +1,9 @@
 print ("*****calculadora******")
 
-n1= input("\nIngrese un numero:")
-n2= input("Ingrese otro numero:")
-
+try:
+    n1= int (input("\nIngrese un numero:"))
+    n2= int (input("Ingrese otro numero:"))
+except:
+    print("Ocurrio un error")
 
 print(f"el resultado es {n1+n2}")
