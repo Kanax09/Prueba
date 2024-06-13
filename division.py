db9ecd99a60325b4bf17113ad05d5afa419815b8
Kanade <celestialwarrior90000@gmail.com>
2024-06-13 12:06:57 -0400

    Funcion multiplicar y dividir añadidas

---
+++ division.py
@@ -0,0 +1,9 @@
+print ("*****Division******")
+
+try:
+    n1= int (input("\nIngrese un numero:"))
+    n2= int (input("Ingrese otro numero:"))
+except:
+    print("Ocurrio un error")
+
+print(f"el resultado es {n1/n2}")
